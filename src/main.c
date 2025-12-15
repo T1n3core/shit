6ca2bb9a5db3d52af79ecfd7de3513ea390e7883
T1n3core <tine.stakul@gmail.com>
2025-12-15 01:57:58 +0100

    aleksander fix this

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
@@ -10,25 +11,29 @@
 
 int create_dir(const char *path);
 
+// TODO: test this and turn it into an init function instead of a giant mess
+// inside main, also add subdirectory creation, gonna have to look up how git
+// does it.
 int main(int argc, char **argv) {
-  if (argv[1].strcmp("init") == 0) {
+  if (strcmp(argv[1], "init") == 0) {
     switch (create_dir(".git")) {
-      case 0: {
-        printf("Successfully created .git/\n");
-        break;
-      }
-      case 1: {
-        printf("Failed to create .git/\n.git/ already exists\n");
-        break;
-      }
-      case -2: {
-        printf("Failed to create .git/\n.git/ already exists, but it is not a directory\n");
-        break;
-      }
-      default: {
-        printf("Failed to create .git/\n");
-        break;
-      }
+    case 0: {
+      printf("Successfully created .git/\n");
+      break;
+    }
+    case 1: {
+      fprintf(stderr, "Failed to create .git/\n.git/ already exists\n");
+      break;
+    }
+    case -2: {
+      fprintf(stderr, "Failed to create .git/\n.git/ already exists, but it is "
+                      "not a directory\n");
+      break;
+    }
+    default: {
+      fprintf(stderr, "Failed to create .git/\n");
+      break;
+    }
     }
   }
 
@@ -39,21 +44,21 @@ int create_dir(const char *path) {
   struct stat st;
   if (stat(path, &st) == 0) {
     if (st.st_mode & S_IFDIR) {
-        return 1; // directory exists
+      return 1; // directory exists
     } else {
-        return -2; // existsts, but not a directory
+      return -2; // existsts, but not a directory
     }
   }
 
-  #ifdef _WIN32
-    if (mkdir(path) == 0) {
-      return 0; //success
-    }
-  #else
-    if (mkdir(path, 0755) == 0) {
-      return 0; //success
-    }
-  #endif
+#ifdef _WIN32
+  if (mkdir(path) == 0) {
+    return 0; // success
+  }
+#else
+  if (mkdir(path, 0755) == 0) {
+    return 0; // success
+  }
+#endif
 
-  return -1; //failure
+  return -1; // failure
 }
